44c90985c903c7c376eb339e50c08714da8669f9
Tanvi Radia <97345560+20CS067@users.noreply.github.com>
2022-08-29 12:29:28 +0530

    Add files via upload

---
+++ FSD Prac5/b/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className='home'>
+      <div>
+      <h1>Welcome to home page</h1>
+      <p>-By Tanvi Radia (20CS067)</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
